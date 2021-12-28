483b99d124e65c9245eb120b34c7e59242855e1c
zhangshizuan <71677939+zhangshizuan@users.noreply.github.com>
2021-12-28 23:49:10 +0800

    pull commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -7,6 +7,6 @@ public class GitTest {
         System.out.println("hello git3!");
         System.out.println("这是hot-fix分支");
         System.out.println("push");
-        System.out.println("pull");
+        System.out.println("这里是pull");
     }
 }
