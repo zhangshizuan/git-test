cba3f1f81adbc154847b529a3833da6547a6a9e9
zhangshizuan <71677939+zhangshizuan@users.noreply.github.com>
2021-12-28 23:48:43 +0800

    Update GitTest.java

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("hello git3!");
         System.out.println("这是hot-fix分支");
         System.out.println("push");
+        System.out.println("pull");
     }
 }
